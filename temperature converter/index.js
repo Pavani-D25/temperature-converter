fedb581a919af3593d4bb151c3acd83a90eb1f15
Pavani-D25 <148316733+Pavani-D25@users.noreply.github.com>
2024-04-07 21:39:22 +0530

    Add files via upload

---
+++ temperature converter/index.js
@@ -0,0 +1,25 @@
+const textbox = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+function convert(){
+    if(toFahrenheit.checked){
+        temp=Number(textbox.value);
+        temp=temp*9/5+32;
+        result.textContent=temp.toFixed(1)+"F";
+       
+    }
+    else if(toCelcius.checked){
+        temp=Number(textbox.value);
+        temp=(temp-32)*(5/9);
+        result.textContent=temp.toFixed(1)+"C";
+
+    }
+    else{
+        result.textContent="Select a unit";
+        
+    }
+   
+
+}
